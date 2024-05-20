b32e1104fc4090779071f08c1083b773d8df7a8a
ahmadahad009 <ahmadahad283@gmail.com>
2024-05-20 16:16:22 +0530

    Added Todo App

--- todo list/Todo.js
+++ todo list/Todo.js
@@ -6,8 +6,12 @@ function Add(){
         alert("Please Enter Task")
     }else{
         let newEle = document.createElement("ul");
-        newEle.innerHTML=`${inputs.value}`;
+        newEle.innerHTML=`${inputs.value} <i class="fa-solid fa-trash"></i>`;
         text.appendChild(newEle);
         inputs.value="";
+        newEle.querySelector("i").addEventListener("click" , remove);
+        function remove(){
+            newEle.remove()
+        }
     }
 }
